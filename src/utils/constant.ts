c68223b1c5b21bda6d251f46ebad5158e91a8bf5
Devendra Mishra <devendra@mavonic.com>
2025-11-27 15:53:35 +0530

    fix(auth): fix auth url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -4,7 +4,8 @@ export const DATABASE_URL: string =
   process.env.DATABASE_URL ??
   'postgresql://postgres:postgres@localhost:5432/press0';
 
-export const BASE_URL: string = process.env.BASE_URL ?? 'http://localhost:3000';
+export const BASE_URL: string =
+  process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
 
 export const LAST_LEGAL_UPDATE_DATE: string = format(
   new Date(2025, 10, 27),
